5e1a441b581b92d7002763f72145fbf1c270f10b
snolly <kassnollygv4462>
2018-02-04 17:32:07 +0300

    first multiscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ declare var $;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','./app.component.media.css']
 })
 
 export class AppComponent {
